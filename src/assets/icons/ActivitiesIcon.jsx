e23c34fb5cec01d7351e03973e9df5343caa0d10
Alongedaniel <alongedaniel41@gmail.com>
2024-04-23 14:47:25 +0100

    menu

---
+++ src/assets/icons/ActivitiesIcon.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ActivitiesIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7.32812 14.4917L9.70813 11.4017C10.0481 10.9617 10.6781 10.8817 11.1181 11.2217L12.9481 12.6617C13.3881 13.0017 14.0181 12.9217 14.3581 12.4917L16.6681 9.51172"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default ActivitiesIcon
--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -4,15 +4,34 @@ import { getNavbarTitle } from "../../utils";
 import { CiBellOn } from "react-icons/ci";
 import { LuUser2 } from "react-icons/lu";
 import { useLocation } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Menu, MenuItem, Paper, Typography } from "@mui/material";
 import BreadcrumbNavigation from "../BreadcrumbNavigation";
 import NotificationIcon from "../../assets/icons/NotificationIcon";
 import ProfileIcon from "../../assets/icons/ProfileIcon";
 import ArrowDown from "../../assets/icons/ArrowDown";
+import Profile from "../../assets/icons/Profile";
+import ActivitiesIcon from "../../assets/icons/ActivitiesIcon";
+import SecurityIcon from "../../assets/icons/SecurityIcon";
+import ArrowForwardIcon from "../../assets/icons/ArrowForwardIcon";
 
 function Navbar({navbarTitle}) {
   const location = useLocation();
-
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const menuItems = [
+    {
+      icon: <Profile />,
+      label: "Profile Information",
+    },
+    {
+      icon: <ActivitiesIcon />,
+      label: "Account Activities",
+    },
+    {
+      icon: <SecurityIcon />,
+      label: "Account Security",
+    },
+  ];
 
   return (
     <Box
@@ -67,14 +86,97 @@ function Navbar({navbarTitle}) {
               sx={{ top: 0, right: 0, borderRadius: "100%" }}
             ></Box>
           </Box>
-          <Box display="flex">
-            <ProfileIcon />
-            <Box>
-              <ArrowDown />
+          <Box sx={{ bgcolor: open ? "#FFFBFE" : "transparent" }}>
+            <Box
+              display="flex"
+              onClick={(e) => {
+                if (open) setAnchorEl(null);
+                else setAnchorEl(e.currentTarget);
+              }}
+              sx={{
+                cursor: "pointer",
+                p: "10px",
+                borderRadius: open ? "100px" : 0,
+              }}
+              bgcolor={open ? "#6750A414" : "transparent"}
+            >
+              <Box>
+                <ProfileIcon />
+              </Box>
+              <Box
+                sx={{
+                  transform: open ? "rotate(180deg)" : "rotate(0deg)",
+                  transition: ".2s linear",
+                }}
+              >
+                <ArrowDown />
+              </Box>
             </Box>
           </Box>
         </div>
       </Box>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={() => setAnchorEl(null)}
+        anchorPosition={{ vertical: "bottom", horizontal: "left" }}
+        PaperProps={{
+          sx: {
+            borderRadius: "20px",
+            width: "320px",
+            // Add any other styles or overrides here
+          },
+        }}
+        sx={{
+          top: "10px",
+          left: "-20px",
+          m: 0,
+          p: 0,
+          "& .MuiMenuItem-root": {
+            padding: "16px",
+            margin: 0,
+          },
+          "& .MuiMenu-paper": {
+            padding: 0,
+            margin: 0,
+            bgcolor: "#FFFBFE",
+          },
+        }}
+      >
+        <Box bgcolor="#6750A414">
+          {menuItems.map((item, i) => (
+            <MenuItem m={0} key={i} onClick={() => setAnchorEl(null)}>
+              <Box width="100%" display="flex" gap="16px" alignItems="center">
+                {item.icon}
+                <Typography fontSize="16px" color="#1C1B1F" flex={1}>
+                  {item.label}
+                </Typography>
+                <ArrowForwardIcon />
+              </Box>
+            </MenuItem>
+          ))}
+          <MenuItem onClick={() => setAnchorEl(null)}>
+            <Button
+              variant="containd"
+              sx={{
+                width: "280px",
+                height: "40px",
+                borderRadius: "100px",
+                bgcolor: "#6750A4",
+                color: "#fff",
+                fontWeight: 500,
+                fontSize: "14px",
+                textTransform: "none",
+                "&:hover": {
+                  bgcolor: "#6750A4",
+                },
+              }}
+            >
+              Logout
+            </Button>
+          </MenuItem>
+        </Box>
+      </Menu>
     </Box>
   );
 }
