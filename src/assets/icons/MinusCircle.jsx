a29db482f83e19f2d91a00bd415dc79bfa041771
Alongedaniel <alongedaniel41@gmail.com>
2024-04-25 18:26:05 +0100

    tracking

---
+++ src/assets/icons/MinusCircle.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MinusCircle = () => {
+  return (
+    <svg
+      style={{ position: "relative", left: "-10px" }}
+      width="25"
+      height="24"
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.5 2C6.99 2 2.5 6.49 2.5 12C2.5 17.51 6.99 22 12.5 22C18.01 22 22.5 17.51 22.5 12C22.5 6.49 18.01 2 12.5 2ZM16.42 12.75H8.42C8.01 12.75 7.67 12.41 7.67 12C7.67 11.59 8.01 11.25 8.42 11.25H16.42C16.83 11.25 17.17 11.59 17.17 12C17.17 12.41 16.84 12.75 16.42 12.75Z"
+        fill="#CAC4D0"
+      />
+    </svg>
+  );
+}
+
+export default MinusCircle
---
+++ src/assets/icons/TrackItemIcon.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const TrackItemIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
+        stroke="#79747E"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9 9.50781L12 6.50781L15 9.50781"
+        stroke="#79747E"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12 6.50781V14.5078"
+        stroke="#79747E"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M6 16.5078C9.89 17.8078 14.11 17.8078 18 16.5078"
+        stroke="#292D32"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default TrackItemIcon
