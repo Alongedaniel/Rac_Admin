176e2394e9e32d1e696ef132d94e12d924d55cd6
Alongedaniel <alongedaniel41@gmail.com>
2024-08-01 16:48:57 +0100

    procurement cost

--- src/utils/hooks/api/axiosInstance.js
+++ src/utils/hooks/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "https://rac-backend.onrender.com/api"
+  baseURL: "https://rac-logistics-api-v1.onrender.com/api",
 });
 
 
